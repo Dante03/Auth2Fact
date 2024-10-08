14807d04e54a13e8ff88111fe2fd38d721a633df
Gerardo Reyes <gerardo.reyes@tuidentidad.com>
2024-10-08 15:24:44 -0600

    Connectionstring

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
-string conn = "Server=localhost;Database=myTest2Fact;User=root;Password=casimir0;";
+string conn = "Server=localhost;Database=myTest2Fact;User=root;Password=;";
 builder.Services.AddDbContext<DataBaseContext>(options => options.UseMySql(conn,
 new MariaDbServerVersion(new Version(10, 2))));
 builder.Services.AddEndpointsApiExplorer();
